95b3f783d237f1b9df9078fc6d2e76e0219090f9
Eric Roy <ericsroy@gmail.com>
2013-07-17 14:43:33 -0700

    Shader compiler in python

--- tools/toolchain/compilers/compile_shader.py
+++ tools/toolchain/compilers/compile_shader.py
@@ -9,64 +9,100 @@ from .. import CONFIG
 from .. import util
 from .. import doc
 
-def compile(src, dst, *args):
-    cgc = os.path.expandvars('$MAKI_DIR/tools/cgc.exe')
-    binary = '0'
+API = 'd3d'
+CGC = os.path.expandvars('$MAKI_DIR/tools/cgc.exe')
+ASSETS_PATH = os.path.join(CONFIG['project_root'], CONFIG['assets_path'])
 
+def _cgc(api, shader_type, variant, shader):
+    cmd = [CGC, '-q', '-O3']       #, '-dumpinputbind', 'bind.txt']
+    if api == 'd3d':
+        cmd.append('-d3d')
+    cmd += ['-entry', shader['entry_point'][0]]
+    target = shader['target'][0]
+    if api == 'ogl':
+        target = 'glsl' + ('f' if shader_type == 'pixel_shader' else 'v')
+    cmd += ['-profile', target]
+
+    defs = shader['variants'][variant]
+    assert len(defs) % 2 == 0
+    for i in range(0, len(defs), 2):
+        cmd.append('-D%s=%s' % (defs[i], defs[i+1]))
+    
+    filename = os.path.normpath(os.path.join(ASSETS_PATH, shader['file_name'][0]))
+    
+    output_file = tempfile.NamedTemporaryFile(delete=False)
+    output_file.close()
+    
+    cmd += ["-o", output_file.name]
+    cmd.append(filename)
+    #print(cmd)
+    subprocess.check_call(cmd)
+
+    with open(output_file.name, 'rb') as file:
+        data = file.read()
+    os.remove(output_file.name)
+    return data
+
+def _parse_line(line):
+    line = line.strip()
+    if not line:
+        return None, None
+    keyvals = line.split(" ", 1)
+    if len(keyvals) > 1:
+        key = keyvals[0]
+        vals = keyvals[1].split()
+    else:
+        key = keyvals[0]
+        vals = []
+    return key, vals
+
+def _leading_spaces(line):
+    return len(line.rstrip()) - len(line.strip())
+
+def _read_shader_defs(filename):
     ps_lines = {}
+    ps_variants = {'': []}
     vs_lines = {}
+    vs_variants = {'': []}
+    
+    with open(filename) as file:
+        lines = list(file)
+    
     line_buffer = None
-    with open(src) as file:
-        for line in file:
-            if line.startswith('vertex_shader'):
-                line_buffer = vs_lines
-            elif line.startswith('pixel_shader'):
-                line_buffer = ps_lines    
-            assert line_buffer != None, "invalid mshad file"
-            line = line.strip()
-            if not line:
-                continue
-            keyvals = line.split(" ", 1)
-            if len(keyvals) > 1:
-                key = keyvals[0]
-                vals = keyvals[1].split()
-            else:
-                key = keyvals[0]
-                vals = []
-            line_buffer[key] = vals
+    variant_buffer = None
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        if line.startswith('vertex_shader'):
+            line_buffer, variant_buffer = vs_lines, vs_variants
+        elif line.startswith('pixel_shader'):
+            line_buffer, variant_buffer = ps_lines, ps_variants
+        assert line_buffer != None, "invalid mshad file"
+        key, vals = _parse_line(line)
+        i += 1
+        if not key:
+            continue
+        line_buffer[key] = vals
+        if key == 'variants':
+            leading_spaces = _leading_spaces(line)
+            while i < len(lines) and _leading_spaces(lines[i]) > leading_spaces:
+                # This is a variant
+                key, vals = _parse_line(lines[i])
+                variant_buffer[key] = vals
+                i += 1
 
+    ps_lines['variants'] = ps_variants
+    vs_lines['variants'] = vs_variants
     shaders = {'pixel_shader': ps_lines, 'vertex_shader': vs_lines}
-    assets_path = os.path.join(CONFIG['project_root'], CONFIG['assets_path'])
-    compiled_shaders = {
-        'pixel_shader': {},
-        'vertex_shader': {}
-    }
+    return shaders
+
+def compile(src, dst, *args):
+    shaders = _read_shader_defs(src)
+    compiled_shaders = {'pixel_shader': {}, 'vertex_shader': {}}
 
     for shader_type, shader in shaders.items():
-        for variant in ('', 'shadow', 'depth'):
-            for api in ('d3d', 'ogl'):
-                cmd = [cgc, '-q']
-                if api == 'd3d':
-                    cmd.append('-d3d')
-                cmd += ['-entry', shader['entry_point'][0]]
-                target = shader['target'][0]
-                if api is 'ogl':
-                    target = 'glsl' + ('f' if shader_type is 'pixel_shader' else 'v')
-                cmd += ['-profile', target]
-                
-                filename = os.path.normpath(os.path.join(assets_path, shader['file_name'][0]))
-                
-                output_file = tempfile.NamedTemporaryFile(delete=False)
-                output_file.close()
-                
-                cmd += ["-o", output_file.name]
-                cmd.append(filename)
-                subprocess.check_call(cmd)
-
-                with open(output_file.name, 'rb') as file:
-                    data = file.read()
-                compiled_shaders[shader_type][variant] = data
-                os.remove(output_file.name)
+        for variant in shader['variants']:
+            compiled_shaders[shader_type][variant] = _cgc(API, shader_type, variant, shader)
 
     with open(dst, 'w') as out:
         for shader_type, variants in compiled_shaders.items():
